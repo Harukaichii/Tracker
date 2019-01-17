2d817fc6004b62bf8c7c91c56b35b181eb78dd8d
Huini Chen <h445chen@edu.uwaterloo.ca>
2019-01-16 23:15:24 -0500

    added form validation and remove button

--- main.js
+++ main.js
@@ -8,54 +8,88 @@ const list = document.getElementById("list"); // list of videos
 
 const allInput = document.querySelectorAll("input"); // array of all the input tags
 
+// makes a new component
+//params : type, attribute(Object), text values
+const createComponent = (type, attribute, textValue) => {
+  let comp = document.createElement(type);
+  let textNode;
+  for (key in attribute) {
+    comp.setAttribute(key, attribute[key]);
+  }
+
+  if (textValue != undefined) {
+    comp.textContent = textValue;
+  }
+
+  return comp;
+};
+
+// adds children to parent component
+const addChildren = (parent, ...children) => {
+  for (let i = 0; i < children.length; i++) {
+    parent.appendChild(children[i]);
+  }
+};
+
+const addFormValidate = (link, title, currVid, totalVid) => {
+  return link.length > 0 && title.length > 0 && +currVid <= +totalVid;
+};
+
 // creates one element to be added to "list"
-function createListElement() {
-  let title_anchor = document.createElement("a"); // sets the link part
-  title_anchor.setAttribute("href", allInput[0].value);
-  title_anchor.textContent = allInput[1].value;
+const createListElement = (link, title, currVid, totalVid) => {
+  let title_anchor = createComponent("a", { href: link }, title);
 
   //sets the title part
-  let currSpan = document.createElement("span");
-  currSpan.textContent = allInput[2].value;
+  let currSpan = createComponent("span", {}, currVid);
 
   // sets the counting part
-  let p = document.createElement("p");
-  let text_node = document.createTextNode(` out of ${allInput[3].value}`);
-  p.appendChild(currSpan);
-  p.appendChild(text_node);
+  let p = createComponent("p");
+  let text_node = document.createTextNode(` out of ${totalVid}`);
+  addChildren(p, currSpan, text_node);
 
-  //increment or decrements the number of current videos watched
-  let plus = document.createElement("button");
-  plus.textContent = "+";
-  plus.addEventListener("click", function() {
-    currSpan.textContent = parseInt(currSpan.textContent) + 1;
+  let plus = createComponent("button", {}, "+");
+  plus.addEventListener("click", () => {
+    if (+currVid < +totalVid) {
+      currVid = +currVid + 1;
+      currSpan.textContent = currVid;
+    }
   });
 
-  let minus = document.createElement("button");
-  minus.textContent = "-";
-  minus.addEventListener("click", function() {
-    currSpan.textContent = parseInt(currSpan.textContent) - 1;
+  let minus = createComponent("button", {}, "-");
+  minus.addEventListener("click", () => {
+    if (+currVid > 1) {
+      currVid = +currVid - 1;
+      currSpan.textContent = currVid;
+    }
   });
 
-  let li = document.createElement("li");
-  li.appendChild(title_anchor);
-  li.appendChild(p);
-  li.appendChild(plus);
-  li.appendChild(minus);
+  let li = createComponent("li");
+  let remove = createComponent("button", { id: "remove" }, "X");
+  remove.addEventListener("click", function() {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+  });
+
+  addChildren(li, remove, title_anchor, p, plus, minus);
 
   return li;
-}
+};
 
 add.addEventListener("click", function() {
   addPrompt.style.display = "block";
-
-  // takes the li and add it to createListElement
 });
 
 done_add.addEventListener("click", function() {
-  addPrompt.style.display = "none";
-  let li = createListElement();
-  list.appendChild(li);
+  const link = allInput[0].value;
+  const title = allInput[1].value;
+  const currVid = allInput[2].value;
+  const totalVid = allInput[3].value;
+  if (addFormValidate(link, title, currVid, totalVid)) {
+    addPrompt.style.display = "none";
+    let li = createListElement(link, title, currVid, totalVid);
+    list.appendChild(li);
+  } else {
+    // display error
+  }
 });
 
 /*
